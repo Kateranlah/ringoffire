aca8d3169c201be33c00549a2498eb5f9176d407
Kevin Sven Lentz <kevin.s.lentz@web.de>
2023-01-13 13:27:07 +0100

    shows curen Player

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { Game } from 'src/models/game'
-import { MatDialog} from '@angular/material/dialog'
+import { MatDialog } from '@angular/material/dialog'
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component'
-
 
 @Component({
   selector: 'app-game',
@@ -27,21 +26,21 @@ export class GameComponent implements OnInit {
     if (!this.pickCardAnimation) {
       this.currentCard = this.game.stack.pop()
       this.pickCardAnimation = true
-   
+      this.game.currenPlayer = this.game.currenPlayer + 1
+      this.game.currenPlayer = this.game.currenPlayer % this.game.players.length
       setTimeout(() => {
-        this.pickCardAnimation = false  
+        this.pickCardAnimation = false
         this.game.playedCards.push(this.currentCard)
       }, 1000)
     }
   }
 
   openDialog(): void {
-    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent)
 
     dialogRef.afterClosed().subscribe((name: string) => {
-      console.log('The dialog was closed', name);
-      this.game.players.push(name);
-    
-    });
+      console.log('The dialog was closed', name)
+      this.game.players.push(name)
+    })
   }
 }
